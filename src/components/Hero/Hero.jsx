7680bda6a568a9dc445bcfd5d2d067896c446151
Charles <charleschuez@utp.ac.pa>
2025-05-02 01:29:28 -0500

    Curso completado y agregado animaciones a la página

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Personaje from "../../assets/Personaje.png"
 import Logo from "../../assets/Logo.png"
 import {motion} from "framer-motion"
+import { slipeUp, slipeInFromSide } from '../../utility/animation'
 
 const Hero = () => {
   return (
@@ -9,19 +10,34 @@ const Hero = () => {
 		<div className='grid grid-cols-1 md:grid-cols-2'>
 			{/* Textos y descripción */}
 			<div className='p-10 sm:p-10 md:p-15 lg:p-30 xl:p-36'>
-				<img src={Logo} alt='Logo del Juego'/>
-				<p className='py-12 text-white'>
+				<motion.img
+				src={Logo} alt='Logo del Juego'
+				variants={slipeUp(0.2)}
+				initial="initial"
+				animate="animate"
+				/>
+				<motion.p
+				className='py-12 text-white'
+				variants={slipeUp(0.3)}
+				initial="initial"
+				animate="animate"
+				>
 					Magic Quest es emocionante juego de aventuras en un
 					mundo de fantasía donde te embarcas en una épica
 					búsqueda mágica. Explora reinos misteriosos, domina
 					poderosos hechizos y enfrentate a criaturas legendarias
 					mientras desvelas secretos ocultos y te convirtes en el
 					héroe supremo. ¡La magia está en tus manos!
-				</p>
-				<div className='flex justify-center gap-4'>
+				</motion.p>
+				<motion.div
+				className='flex justify-center gap-4'
+				variants={slipeUp(1)}
+				initial="initial"
+				animate="animate"
+				>
 					<a
 					className='bg-purple-600 px-12 py-2 rounded-3xl text-white hover:bg-purple-700
-					transition-all duration-300 items-center'
+					transition-all duration-300 items-center cursor-pointer'
 					>
 						Jugar ahora
 						<i className="bi bi-controller text-xl ml-2"></i>
@@ -32,13 +48,18 @@ const Hero = () => {
 						Ver Gameplay
 						<i className="bi bi-youtube text-xl ml-2"></i>
 					</a>
-				</div>
+				</motion.div>
 			</div>
 
 			{/* Imagen */}
-			<div className='p-10 sm:p-10 md:p-15 lg:p-30 xl:p-36'>
+			<motion.div
+			className='p-10 sm:p-10 md:p-15 lg:p-30 xl:p-36'
+			variants={slipeInFromSide("right", 0.5)}
+			initial="initial"
+			animate="animate"
+			>
 				<img src={Personaje} alt='Personaje del Juego'/>
-			</div>
+			</motion.div>
 		</div>
 	</section>
   )
